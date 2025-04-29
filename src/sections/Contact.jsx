0dc781858f3b9a8d125d69fa6789309f7addf2c2
Shlok Motwani <shlok.codes@gmail.com>
2025-04-29 14:43:01 +0530

    fix: add form validation constraints

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -8,15 +8,23 @@ export default function Contact() {
         <form action="" method="POST" id="contact-form">
           <div>
             <label htmlFor="name">Full Name: </label>
-            <input type="text" name="name" id="name" />
+            <input type="text" name="name" id="name" required />
           </div>
           <div>
             <label htmlFor="email">Email Address: </label>
-            <input type="email" name="email" id="email" />
+            <input type="email" name="email" id="email" required />
           </div>
           <div>
             <label htmlFor="contact-number">Contact Number: </label>
-            <input type="number" name="contact-number" id="contact-number" />
+            <input
+              type="tel"
+              pattern="[0-9]{10}"
+              minLength="10"
+              maxLength="10"
+              name="contact-number"
+              id="contact-number"
+              required
+            />
           </div>
           <div id="query-div">
             <textarea
